5c8958191103199bff816de98fd558d460a0733e
Khang <kt36@njit.edu>
2023-10-23 03:23:59 +0300

    whitebox attack

--- Data/read.py
+++ Data/read.py
@@ -68,7 +68,7 @@ def read_data(args, history, exist=False):
         graph.ndata['te_mask'] = torch.LongTensor(id_te)
         graph.ndata['sh_mask'] = torch.LongTensor(id_sh)
 
-    tar_g, sha_g = remove_edge(graph=graph, mode=args.gen_submode)
+    tar_g, sha_g = remove_edge(graph=graph, mode=args.att_mode, submode=args.gen_submode)
     console.log(f"Done splitting train/val/test: :white_check_mark:")
 
     args.num_class = len(list_of_label)
@@ -105,7 +105,6 @@ def read_data(args, history, exist=False):
         x = g_te.ndata['feat']
         y = g_te.ndata['label']
         nodes = g_te.nodes()
-        print("Hello this is nodes of test:", nodes)
         src_edge, dst_edge = g_te.edges()
         prop_test = {
             '# nodes': f'{nodes.size(dim=0)}',
