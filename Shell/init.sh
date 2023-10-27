e5a7a6bbbae9d29c4180003045d514ab6a3a4742
Khang <kt36@njit.edu>
2023-10-27 11:38:44 +0300

    add dockerfile

--- Shell/init.sh
+++ Shell/init.sh
@@ -3,6 +3,17 @@ mkdir results/models/
 mkdir results/dict/
 mkdir results/logs/
 
-pip install wandb loguru numpy scipy rich tqdm matplotlib 
+pip install wandb
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+
+pip install  dgl -f https://data.dgl.ai/wheels/cu118/repo.html
+pip install  dglgo -f https://data.dgl.ai/wheels-test/repo.html
+
+pip install torch_geometric
+pip install numpy scipy rich tqdm matplotlib
 pip install torchmetrics
-conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia -y
+pip install networkx
+pip install ogb
+pip install \
+    --extra-index-url=https://pypi.nvidia.com \
+    cudf-cu11 dask-cudf-cu11 cuml-cu11 cugraph-cu11 cuspatial-cu11 cuproj-cu11 cuxfilter-cu11 cucim
