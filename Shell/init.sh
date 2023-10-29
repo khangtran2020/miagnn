7ac262a710b8ff73e9eb0aa5d0a6e2dc8b69fc07
Khang <kt36@njit.edu>
2023-10-29 14:16:05 +0300

    modification

--- Shell/init.sh
+++ Shell/init.sh
@@ -4,7 +4,7 @@ mkdir results/dict/
 mkdir results/logs/
 
 pip install wandb
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
 
 pip install  dgl -f https://data.dgl.ai/wheels/cu118/repo.html
 pip install  dglgo -f https://data.dgl.ai/wheels-test/repo.html
@@ -15,6 +15,3 @@ pip install numpy scipy rich tqdm matplotlib
 pip install torchmetrics
 pip install networkx
 pip install ogb
-# pip install \
-#     --extra-index-url=https://pypi.nvidia.com \
-#     cudf-cu11 dask-cudf-cu11 cuml-cu11 cugraph-cu11 cuspatial-cu11 cuproj-cu11 cuxfilter-cu11 cucim
