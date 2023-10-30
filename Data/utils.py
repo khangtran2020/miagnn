526101b235897aca2a85e86540a46538e1e3e8e8
Khang <kt36@njit.edu>
2023-10-30 11:18:30 +0300

    modification

--- Data/utils.py
+++ Data/utils.py
@@ -309,7 +309,7 @@ def shadow_visualization(graph:dgl.DGLGraph, path:str):
     if os.path.exists(path=path):
         pos = read_pickel(path)
     else:
-        pos = nx.spring_layout(G, iterations=200)
+        pos = nx.spring_layout(G)
         save_dict(path=path, dct=pos)
 
     plt.figure(num=None, figsize=(15, 15))
